593a01f933c636f9924e916ac0a074ca4a9f513c
Nicolas Nieloux <nicolas.nieloux@le-campus-numerique.fr>
2023-05-05 12:00:12 +0200

    modify profil & hashpassword

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
@@ -29,27 +30,39 @@ class DashboardController extends AbstractController
     }
 
     #[Route('/dashboard/{id}/edit', name: 'app_dashboard_edit', methods: ['GET', 'POST'])]
-    public function edit(EntityManagerInterface $entityManager, int $id, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    public function edit(
+        EntityManagerInterface $entityManager,
+        int $id,
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        DuckRepository $duckRepository): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $duck = $entityManager->getRepository(Duck::class)->find($id);
         $duck->setFirstname('');
         $duck->setLastname('');
         $duck->setPassword('');
 
         $form = $this->createFormBuilder($duck)
-            ->add('firstname', TextType::class )
+            ->add('firstname', TextType::class)
             ->add('lastname', TextType::class)
             ->add('password', PasswordType::class)
             ->add('save', SubmitType::class, ['label' => 'Modify Profile'])
             ->getForm();
         $form->handleRequest($request);
 
-        if (!$duck) {
-            throw $this->createNotFoundException(
-                'No duck found for id '.$id
+      //  if($form->isSubmitted() && $form->isValid()) {
+            // On enregistre le nouveau mot de passe en le hashant
+            $duck->setPassword(
+                $passwordHasher->hashPassword(
+                    $duck,
+                    $form->get('password')->getData()
+                )
             );
-        }
-        $entityManager->flush();
+            $entityManager->flush();
+
+//            return $this->redirectToRoute('app_dashboard_edit');
+ //       }
 
         return $this->render('dashboard/edit.html.twig', [
             'duck' => $duck,
